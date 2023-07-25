d28eb3a8f41fce9c3bbbe90aa82a37b87dbe4055
xvsvg <pobedotriples@gmail.com>
2023-07-25 17:09:50 +0300

    feat: compilation submission handler

---
+++ xMap/Generation/Sources/CompilationHandlingLink.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using xMap.Tools.Contracts;
+
+namespace xMap.Generation.Sources;
+
+internal class CompilationHandlingLink : ILink
+{
+    public ILink Next { get; set; } = null!;
+
+    public void Process(SourceProductionContext context, SemanticModel semanticModel, CompilationUnitSyntax source)
+    {
+        context.AddSource("MapperExtensions.g.s", source.GetText(Encoding.UTF8));
+        Next?.Process(context, semanticModel, source);
+    }
+}
