e486e579e4f41d99654c0786a71a0cbcdfcb54f9
xvsvg <pobedotriples@gmail.com>
2023-07-25 17:10:25 +0300

    feat: basic generation test

---
+++ xMap.Tests/GenerationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using xMap.Generation.Generator;
+using xMap.Tests.Tools;
+
+namespace xMap.Tests;
+
+public class GenerationTests
+{
+    [Test]
+    public async Task ProvideValidSource_ShouldGenerate()
+    {
+        var source = await File.ReadAllTextAsync(@"../../../../xMap.Sample/Receiver.cs");
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var refs = new[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+        };
+
+        var compilation = CSharpCompilation.Create(
+            typeof(object).Assembly.FullName,
+            new[] { syntaxTree },
+            refs);
+
+        var updatedCompilation = TestHelper.RunGenerators(
+            compilation,
+            out var diagnostics,
+            new Generator());
+
+        var files = updatedCompilation.SyntaxTrees
+            .Where(t => Path.GetFileName(t.FilePath) is not null);
+
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+            Console.WriteLine(new string('-', 30));
+        }
+    }
+}
---
+++ xMap.Tests/Tools/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace xMap.Tests.Tools;
+
+public static class TestHelper
+{
+    public static Compilation RunGenerators(
+        Compilation compilation,
+        out ImmutableArray<Diagnostic> diagnostics,
+        IIncrementalGenerator generator)
+    {
+        CSharpGeneratorDriver
+            .Create(generator)
+            .RunGeneratorsAndUpdateCompilation(compilation, out var newCompilation, out diagnostics);
+
+        return newCompilation;
+    }
+}
